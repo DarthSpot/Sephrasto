78f64e0652ebfcd82444e3476f5fcf4965f6c674
Alexander Mills <spot.enterprise@web.de>
2022-08-26 12:05:57 +0200

    Cleanup

--- src/Sephrasto/Sephrasto.py
+++ src/Sephrasto/Sephrasto.py
@@ -337,11 +337,10 @@ QTreeView::item {{ margin-top: 4px; margin-bottom: 4px; }}
 .h3, QGroupBox {{ font-weight: bold; font-variant: small-caps; font-size: {Wolke.FontHeadingSizeL3}pt; {standardFont}; color: {Wolke.HeadingColor}; }}
 .h4 {{ font-weight: bold; }}
 .title {{ font-weight: bold; font-size: 16pt; {headingFont}; }}
-.icon {{ font-size: {min(Wolke.Settings['FontSize'], 12)}pt; font-family: 'Font Awesome 6 Free Solid'; font-weight: 'Solid'}}\n"""
+.icon {{ font-size: {min(Wolke.Settings['FontSize'], 12)}pt; font-family: 'Font Awesome 6 Free Solid'; font-weight: 900; }}\n"""
 
         if 'CSS' in theme:
             css += theme['CSS']
-        css += ".icon { font-weight: 900; }\n"
         self.app.setStyleSheet(css)
         
 if __name__ == "__main__":
